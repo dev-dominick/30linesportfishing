de76c307d4d75972d2c93573725323c3b28f09c5
Dom <Domalbano35@gmail.com>
2023-06-30 21:16:51 -0400

    init

---
+++ src/app/about/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import Nav from "@/components/Nav";
+
+const About = (props) => {
+  return (
+    <>
+      {/* <Nav></Nav> */}
+      <div className="">About</div>
+    </>
+  );
+};
+
+About.propTypes = {};
+
+export default About;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="flex flex-col align-center sticky top-0 sm:h-36 h-20 bg-white">
+      <div className="flex  justify-center border-solid border-red-500 w-50 h-50">
+        <h1 className="text-black">Nav</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Nav
